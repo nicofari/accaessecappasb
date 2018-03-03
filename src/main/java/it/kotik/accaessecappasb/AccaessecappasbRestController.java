bb288cbc4f768667c214a8979461918fe5fd5ac5
nik <nicola.farina64@gmail.com>
2018-03-03 17:20:24 +0100

    Cross origin requests enabled

--- src/main/java/it/kotik/accaessecappasb/AccaessecappasbRestController.java
+++ src/main/java/it/kotik/accaessecappasb/AccaessecappasbRestController.java
@@ -4,6 +4,7 @@ import org.springframework.core.io.ClassPathResource;
 import org.springframework.core.io.InputStreamResource;
 import org.springframework.core.io.Resource;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -14,6 +15,7 @@ import java.io.IOException;
 public class AccaessecappasbRestController {
 
     @RequestMapping(value = "/words", method = RequestMethod.GET, produces = "application/json;charset=UTF-8")
+    @CrossOrigin
     public ResponseEntity<InputStreamResource> getWords() throws IOException {
         Resource r = new ClassPathResource("static/words.json");
 
